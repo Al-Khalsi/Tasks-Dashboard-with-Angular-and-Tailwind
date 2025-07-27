2cd57f3e7afbd0bdc9cf62b84d2fce9f413c18c5
Al-Khalsi <mohammad.hassan.alkhalsi@gmail.com>
2025-07-27 22:30:52 +0330

    refactor(routing): update routes for auth system
    - Add login/register routes
    - Protect main routes with auth guard
    - Add logout route
    - Update lazy loading paths

--- src/app/app.routes.js
+++ src/app/app.routes.js
@@ -1,27 +1,39 @@
 import { DashboardComponent } from './pages/dashboard/dashboard.component.js';
 import { TaskNewComponent } from './pages/task-new/task-new.component.js';
 import { TaskEditComponent } from './pages/task-edit/task-edit.component.js';
+import { authGuard } from '../middleware/auth.guard.js';
 
 export const routes = [
-  { 
-    path: 'tasks', 
-    component: DashboardComponent 
+  {
+    path: '',
+    redirectTo: 'tasks',
+    pathMatch: 'full'
   },
-  { 
-    path: 'tasks/new', 
-    component: TaskNewComponent 
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/authentication/login.component').then(m => m.LoginComponent)
   },
-  { 
-    path: 'tasks/:id/edit', 
-    component: TaskEditComponent 
+  {
+    path: 'register',
+    loadComponent: () => import('./pages/authentication/register.component').then(m => m.RegisterComponent)
   },
-  { 
-    path: '', 
-    redirectTo: '/tasks', 
-    pathMatch: 'full' 
+  {
+    path: 'tasks',
+    loadComponent: () => import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent),
+    canActivate: [authGuard]
   },
-  { 
-    path: '**', 
-    redirectTo: '/tasks' 
+  {
+    path: 'tasks/new',
+    loadComponent: () => import('./pages/task-new/task-new.component').then(m => m.TaskNewComponent),
+    canActivate: [authGuard]
+  },
+  {
+    path: 'tasks/:id/edit',
+    loadComponent: () => import('./pages/task-edit/task-edit.component').then(m => m.TaskEditComponent),
+    canActivate: [authGuard]
+  },
+  {
+    path: '**',
+    redirectTo: 'tasks'
   }
 ];
