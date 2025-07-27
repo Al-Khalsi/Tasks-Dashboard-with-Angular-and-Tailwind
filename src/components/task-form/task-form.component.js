7634eb2d603592186098b8cb727d96515fe1c4e3
Al-Khalsi <mohammad.hassan.alkhalsi@gmail.com>
2025-07-27 19:30:57 +0330

    feat(ui): redesign task cards

--- src/components/task-form/task-form.component.js
+++ src/components/task-form/task-form.component.js
@@ -9,29 +9,32 @@ import { FormGroup, FormControl, Validators, FormsModule, ReactiveFormsModule } 
   template: `
     <form [formGroup]="taskForm" (ngSubmit)="onSubmit()" class="space-y-4">
       <div>
-        <label class="block">Title*</label>
+        <label class="block text-gray-300">Title*</label>
         <input 
           type="text" 
           formControlName="title"
-          class="w-full p-2 border rounded">
+          class="w-full p-2 border border-gray-700 rounded bg-gray-800 text-gray-100
+                 focus:outline-none focus:ring-2 focus:ring-blue-400">
         <div *ngIf="taskForm.get('title').invalid && taskForm.get('title').touched" 
-             class="text-red-500 text-sm">
+             class="text-red-400 text-sm mt-1">
           Title is required
         </div>
       </div>
       
       <div>
-        <label class="block">Description</label>
+        <label class="block text-gray-300">Description</label>
         <textarea 
           formControlName="description"
-          class="w-full p-2 border rounded"></textarea>
+          class="w-full p-2 border border-gray-700 rounded bg-gray-800 text-gray-100
+                 focus:outline-none focus:ring-2 focus:ring-blue-400 h-24"></textarea>
       </div>
       
       <div>
-        <label class="block">Assigned User</label>
+        <label class="block text-gray-300">Assigned User</label>
         <select 
           formControlName="assignedUser"
-          class="w-full p-2 border rounded">
+          class="w-full p-2 border border-gray-700 rounded bg-gray-800 text-gray-100
+                 focus:outline-none focus:ring-2 focus:ring-blue-400">
           <option [ngValue]="null">Unassigned</option>
           <option *ngFor="let user of users" [ngValue]="user">
             {{ user.name }}
@@ -44,14 +47,17 @@ import { FormGroup, FormControl, Validators, FormsModule, ReactiveFormsModule } 
           type="checkbox" 
           id="completed"
           formControlName="completed"
-          class="mr-2">
-        <label for="completed">Completed</label>
+          class="mr-2 h-5 w-5 text-blue-400 rounded bg-gray-800 border-gray-700
+                 focus:ring-blue-400 focus:ring-2">
+        <label for="completed" class="text-gray-300">Completed</label>
       </div>
       
       <button 
         type="submit" 
         [disabled]="taskForm.invalid"
-        class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 disabled:bg-gray-400">
+        class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-500 
+               disabled:bg-gray-700 disabled:text-gray-500 transition-colors
+               focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50">
         {{ submitText }}
       </button>
     </form>
