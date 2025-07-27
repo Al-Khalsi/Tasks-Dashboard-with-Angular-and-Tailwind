e3f88a9ffd7ace4c25b5b9c6eb04ff74d0c941cf
Al-Khalsi <mohammad.hassan.alkhalsi@gmail.com>
2025-07-27 12:52:07 +0330

    feat(service): add user fetching functionality

--- src/services/task.service.js
+++ src/services/task.service.js
@@ -1,10 +1,21 @@
 export class TaskService {
-  constructor() {
-    this.tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-    this.users = [];
-  }
+    constructor() {
+        this.tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+        this.users = [];
+    }
 
-  _saveToLocalStorage() {
-    localStorage.setItem('tasks', JSON.stringify(this.tasks));
-  }
+    async fetchUsers() {
+        try {
+            const response = await fetch('https://jsonplaceholder.typicode.com/users');
+            this.users = await response.json();
+            return this.users;
+        } catch (error) {
+            console.error('Error fetching users:', error);
+            return [];
+        }
+    }
+
+    _saveToLocalStorage() {
+        localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    }
 }
