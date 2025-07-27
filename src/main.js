f8d3b5c06c9742b635b42edcb4a0c832615af7bb
Al-Khalsi <mohammad.hassan.alkhalsi@gmail.com>
2025-07-27 20:04:16 +0330

    fix(main.js): imported TaskServise and routes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { AppComponent } from './app/app.component.js';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes.js';
+import { TaskService } from './services/task.service.js';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    TaskService
+  ]
+}).catch(err => console.error(err));
