77cd8a40123420775332244b30a515dea33fed24
Al-Khalsi <mohammad.hassan.alkhalsi@gmail.com>
2025-07-27 16:53:04 +0330

    refactor(task-service): convert to Angular service

--- src/services/task.service.js
+++ src/services/task.service.js
@@ -1,3 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
 export class TaskService {
     constructor() {
         this.tasks = JSON.parse(localStorage.getItem('tasks')) || [];
