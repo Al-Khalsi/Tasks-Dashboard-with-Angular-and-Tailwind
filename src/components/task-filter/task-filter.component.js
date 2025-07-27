2603c2645140d386012fd90cc2661d4a025ae51c
Al-Khalsi <mohammad.hassan.alkhalsi@gmail.com>
2025-07-27 13:20:47 +0330

    feat(components): add TaskFilter component with search and status filtering

---
+++ src/components/task-filter/task-filter.component.js
@@ -0,0 +1,36 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-task-filter',
+  template: `
+    <div class="flex gap-4 mb-6">
+      <select 
+        class="p-2 border rounded"
+        [(ngModel)]="filters.status"
+        (change)="applyFilters()">
+        <option value="">All Statuses</option>
+        <option value="completed">Completed</option>
+        <option value="pending">Pending</option>
+      </select>
+      
+      <input
+        type="text"
+        class="p-2 border rounded flex-grow"
+        placeholder="Search tasks..."
+        [(ngModel)]="filters.searchTerm"
+        (input)="applyFilters()">
+    </div>
+  `
+})
+export class TaskFilterComponent {
+  filters = {
+    status: '',
+    searchTerm: ''
+  };
+
+  @Output() filterChange = new EventEmitter();
+
+  applyFilters() {
+    this.filterChange.emit({ ...this.filters });
+  }
+}
